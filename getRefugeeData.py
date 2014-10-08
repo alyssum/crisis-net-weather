8c094ddaab85e868ffb9a07d54e80fbc08ae157f
Cindy Xiao <c.xiao@live.com>
2014-10-08 15:25:00 -0700

    Getting refugee data from UNHCR as json dump.

---
+++ getRefugeeData.py
@@ -0,0 +1,22 @@
+def getRefugeeData(source='unhcr'):
+    import requests
+    import json
+    """
+    Make an API request from CrisisNET using a specified source.
+    Outputs the data in JSON.
+    """
+    
+    # Make API request
+    apiKey = '5435a0326d18ed0f54deb848'
+    
+    # Construct request URL
+    apiURL = 'http://api.crisis.net/item/?sources=%s' % source
+
+    # Request header
+    headers = {'Authorization': 'Bearer ' + apiKey}
+
+    r = requests.get(apiURL, headers=headers)  
+    return r.json()
+
+    # to print
+    # print json.dumps(r.json(), indent=4, sort_keys=True)
