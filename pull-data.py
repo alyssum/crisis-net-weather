743ab2e51161d4c1c3c7be1357fe8e6f8ea29cf6
mona <jalal@wisc.edu>
2014-10-08 17:07:15 -0700

    “mona”

--- pull-data.py
+++ pull-data.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-
+from microsofttranslator import Translator
+import goslate
 # Import modules
 import pandas as pd
 import requests
@@ -12,7 +13,7 @@ pd.set_option('display.max_row', 1000)
 api_key = '532d8dc4ed3329652f114b73'
 
 # Insert your CrisisNET request API
-api_url = 'http://api.crisis.net/item/?sources=noaa_alerts'
+api_url = 'http://api.crisis.net/item?apikey=532d8dc4ed3329652f114b73&sources=twitter&weather,disaster'
 
 # Create the request header
 headers = {'Authorization': 'Bearer ' + api_key}
@@ -31,6 +32,9 @@ def get_data(offset=0, limit=100, df=None):
     r = requests.get(url, headers=headers)
     # convert the request data into a dataframe,
     x = pd.DataFrame(r.json())
+
+
+
     # expand the dataframe
     x = x['data'].apply(pd.Series)
     # add the dataframe's rows to the main dataframe, df, we defined outside the function
@@ -46,16 +50,47 @@ def get_data(offset=0, limit=100, df=None):
 # Run the function
 df = get_data(df=df)
 
+def run(data):
+    if 'language' in data and 'code' in data['language'] and data['language']['code'] == 'en':
+        return data
+
+
+    """
+    translator = Translator(settings.BING_APP_ID, settings.BING_APP_SECRET)
+
+    try:
+        data['contentEnglish'] = translator.translate(data['content'], "en")
+    except Exception, e:
+        print e
+        pass
+    """
+
+    try:
+      gs = goslate.Goslate()
+      data['contentEnglish'] = gs.translate(data['content'][:1000], 'en')
+    except Exception, e:
+      print e
+      pass
+
+    return data
+
 # Check the number of data points retrieved
 len(df)
 
+translated_df=run(df)
+print translated_df.head()
+
 # Check for duplicate data points
-df['id'].duplicated().value_counts()
+translated_df['id'].duplicated().value_counts()
 
 # Drop all duplicate data points
-df = df.dropna(how='all')
+translated_df = translated_df.dropna(how='all')
+
+translated_df.to_csv('TwitterWeather.csv',encoding='utf-8')
 
 # View the first 10 data points
-df.head()
+#print df.head()
+print df['id'].duplicated().value_counts()
+print len(df)
 
-
+translated_df = translated_df.dropna(how='all')
